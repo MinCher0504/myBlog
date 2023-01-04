7986589f379ae42d2bd20230b1fb76f98439c8e8
MinCher0504 <sdkyu12345@gmail.com>
2023-01-04 12:05:14 +0900

    .

--- Front-Backend/client/src/components/views/Cloth/Cloth.js
+++ Front-Backend/client/src/components/views/Cloth/Cloth.js
@@ -1,27 +1,63 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Cloth.css";
 
 function Cloth() {
-  const [DBdata, setDBdata] = useState([]);
+  const [a, b] = useState(true);
+  let navigate = useNavigate();
+  function onClickHandler() {
+    navigate("/");
+  }
+
+  let x = [];
+  var information = (
+    <div className='contents'>
+      <button onClick={onClickHandler}>돌아가기</button>
+      <ul className='display' />
+    </div>
+  );
+
   useEffect(() => {
-    axios.get("/api/users/cloth").then((res) => {
-      var arr = [];
-      var structure = {
-        name: "",
-        description: "",
-        cost: "",
-        image: "",
-        brand: "",
-      };
-      res.data.forEach((element) => {
-        arr.push(element);
-        console.log(arr);
+    let info = document.getElementsByClassName("display")[0];
+    if (a) {
+      axios.get("/api/users/cloth").then((res) => {
+        res.data.forEach((element) => {
+          const image = document.createElement("img");
+          image.src = element.image;
+          image.style.width = "450px";
+
+          const name = document.createElement("div");
+          name.innerText = element.name;
+
+          const brand = document.createElement("div");
+          brand.innerText = element.brand;
+
+          const cost = document.createElement("div");
+          cost.innerText = element.cost;
+
+          const describe = document.createElement("div");
+          describe.innerText = element.describe;
+
+          const name_cost = document.createElement("div");
+          name_cost.appendChild(name);
+          name_cost.appendChild(cost);
+          name_cost.className = "name_cost";
+
+          const display = document.createElement("div");
+          display.className = "display_content";
+          display.appendChild(image);
+          display.appendChild(name_cost);
+          display.appendChild(brand);
+          display.appendChild(describe);
+          info.appendChild(display);
+        });
       });
-    });
+      b(false);
+    }
   }, []);
 
-  return <div>test</div>;
+  return <div>{information}</div>;
 }
 
 export default Cloth;
